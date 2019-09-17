e9e1fd5cbefe2f43de37974e5988a9d05d005078
Casper Langemeijer <casper.langemeijer@procurios.nl>
2019-09-17 14:26:03 +0200

    New release 1.2

--- php_ssh2.h
+++ php_ssh2.h
@@ -23,7 +23,7 @@
 #include <libssh2_sftp.h>
 #include "ext/standard/url.h"
 
-#define PHP_SSH2_VERSION				"1.1.2"
+#define PHP_SSH2_VERSION				"1.2"
 #define PHP_SSH2_DEFAULT_PORT			22
 
 /* Exported Constants */
